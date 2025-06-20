cf3ca1a67002525407abf8817ad29a247309fd4b
Glenndio-cpu <s22110543@student.unklab.ac.id>
2025-06-20 11:26:38 +0800

    Exercise 8

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SplashScreen from './src/pages/SplashScreen';
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
+import Homepage from './src/pages/HomePage';
+const App = () => {
+  return <Homepage />;
+};
+
+export default App;
--- index.js
+++ index.js
@@ -3,15 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Exercise2 from './Exercise2';
-import Exercise3 from './Exercise3';
-import Exercise4 from './Exercise4';
-import Exercise5 from './Exercise5';
-import Exercise6 from './Exercise6';
-import Exercise7 from './Exercise7';
 import Exercise8 from './Exercise8';
-import Flexbox from './Flexbox';
-import Login from './Login';
+// import Flexbox from './Flexbox';
+import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Exercise6);
+AppRegistry.registerComponent(appName, () => App);
